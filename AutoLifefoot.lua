173792a388e33bac02abe5ba91aa3ad3ad0f1c5d
liiwukangs-star <liiwukangs@gmail.com>
2026-07-09 07:49:51 +0700

    Update AutoLifefoot.lua

--- AutoLifefoot.lua
+++ AutoLifefoot.lua
@@ -2,21 +2,12 @@ local ffi = require("ffi")
 local memory = require("memory")
 local imgui = require("mimgui")
 local gta = ffi.load("GTASA")
-local samem = require "SAMemory"
-local inicfg = require "inicfg"
-local faicons = require('fAwesome6')
+local samem = require("SAMemory")
+local inicfg = require("inicfg")
+local sampev = require("samp.events")
 
 samem.require("CPed")
 samem.require("CPlayerData")
-local imgui  = require "mimgui"
-local samem  = require "SAMemory"
-local sampev = require "samp.events"
-
-samem.require "CPed"
-samem.require "CPlayerData"
-local camera = samem.camera
-local playerPed = samem.player_ped
-local player_ped = samem.cast("CPed **", samem.player_ped)
 
 ffi.cdef[[void _Z12AND_OpenLinkPKc(const char* link);]]
 
@@ -24,9 +15,8 @@ function openLink(url)
     gta._Z12AND_OpenLinkPKc(url)
 end
 
+local player_ped = samem.cast("CPed **", samem.player_ped)
 local window = imgui.new.bool(false)
-
-local inicfg = require "inicfg"
 
 local cfg = inicfg.load({
     Setting = {
@@ -48,7 +38,7 @@ local ResetDuration     = imgui.new.int(cfg.Setting.ResetDuration)
 local EnableWeaponReset = imgui.new.bool(cfg.Setting.EnableWeaponReset)
 local RestoreDelay      = imgui.new.int(cfg.Setting.RestoreDelay)
 
-function saveset()
+local function saveset()
     cfg.Setting.EnableMain        = EnableMain[0]
     cfg.Setting.UseBulletSync     = UseBulletSync[0]
     cfg.Setting.UseGiveDamage     = UseGiveDamage[0]
@@ -60,11 +50,6 @@ function saveset()
     inicfg.save(cfg, "autolifefoot")
     sampAddChatMessage("Config disimpan!", -1)
 end
-
-local fontTitle = nil
-local fontAwesome = nil
-
-local player_ped = samem.cast("CPed **", samem.player_ped)
 
 local function getPed()
     if not player_ped then return nil end
@@ -87,68 +72,60 @@ local resetUntil = 0
 local lastWeapon = nil
 local restoreAt  = 0
 local restoring  = false
-local emptyAt    = 0
-local toEmpty    = false
-local EMPTY_DELAY = 500
-local shotAt = 0
-local hitDetected = false
-local MISS_DELAY = 150
 
 local function triggerReset()
     local now = nowMs()
     resetUntil = now + ResetDuration[0]
     if not EnableWeaponReset[0] then return end
+
     local curWeapon = getCurrentCharWeapon(PLAYER_PED)
     if curWeapon ~= 0 then lastWeapon = curWeapon end
-    if not toEmpty and not restoring then
-        emptyAt = now + EMPTY_DELAY
-        toEmpty = true
-    end
+
+    setCurrentCharWeapon(PLAYER_PED, 0)
+    restoreAt = now + RestoreDelay[0]
+    restoring = true
 end
 
 function sampev.onSendGiveDamage(playerId, damage, weapon, bodypart)
-    if not EnableMain[0] then return end
-    if not UseGiveDamage[0] then return end
+    if not EnableMain[0] or not UseGiveDamage[0] then return end
     if damage > 0 and weapon ~= 0 then
         triggerReset()
     end
 end
 
 function sampev.onSendBulletSync(data)
-    if not EnableMain[0] then return end
-    if not UseBulletSync[0] then return end
-    shotAt = nowMs()
-    hitDetected = false
+    if not EnableMain[0] or not UseBulletSync[0] then return end
+    triggerReset()
 end
 
 lua_thread.create(function()
     while true do
-        wait(0)
-        if EnableMain[0] and resetUntil > nowMs() then
+        local now = nowMs()
+        local sprintActive = EnableMain[0] and resetUntil > now
+        local restoreActive = EnableWeaponReset[0] and restoring
+
+        if sprintActive then
             local ped = getPed()
             if ped then
                 local pdata = getPlayerData()
                 if pdata then
-                    pdata.fSprintEnergy = SprintSpeed[0]
+                    pdata.fSprintEnergy = 5.0
                     pdata.bPlayerSprintDisabled = false
                 end
+                ped.nPedFlags.bResetWalkAnims = 1
             end
         end
-        if EnableWeaponReset[0] and toEmpty and emptyAt > 0 and nowMs() >= emptyAt then
-            setCurrentCharWeapon(PLAYER_PED, 0)
-            restoreAt = nowMs() + RestoreDelay[0]
-            restoring = true
-            emptyAt = 0
-            toEmpty = false
-        end
-        if EnableWeaponReset[0] and restoring and restoreAt > 0 and nowMs() >= restoreAt then
-            if lastWeapon and lastWeapon ~= 0 then
-                setCurrentCharWeapon(PLAYER_PED, lastWeapon)
-            else
-                setCurrentCharWeapon(PLAYER_PED, 24)
-            end
+
+        if restoreActive and restoreAt > 0 and now >= restoreAt then
+            setCurrentCharWeapon(PLAYER_PED, (lastWeapon and lastWeapon ~= 0) and lastWeapon or 24)
             restoreAt = 0
             restoring = false
+        end
+
+        if sprintActive or restoreActive then
+            wait(0)
+        else
+            wait(50)
         end
     end
 end)
@@ -156,56 +133,51 @@ end)
 function main()
     while not isSampAvailable() do wait(0) end
     sampRegisterChatCommand("alm", function() window[0] = not window[0] end)
-    while true do
-        wait(0)
-        if UseBulletSync[0] then
-            if shotAt > 0 and not hitDetected and nowMs() - shotAt >= MISS_DELAY then
-                triggerReset()
-                shotAt = 0
-            end
-            if hitDetected then
-                shotAt = 0
-            end
-        end
-    end
+    wait(-1)
 end
+
+local REF_WIDTH  = 1280.0
+local REF_HEIGHT = 720.0
+
+local function getDPI()
+    local io = imgui.GetIO()
+    local sx = io.DisplaySize.x / REF_WIDTH
+    local sy = io.DisplaySize.y / REF_HEIGHT
+    return math.min(sx, sy)
+end
+
+local DPI = getDPI()
 
 imgui.OnFrame(
     function()
         return window[0]
     end,
     function()
-        imgui.SetNextWindowSize(imgui.ImVec2(199, 0), imgui.Cond.FirstUseEver)
+        imgui.SetNextWindowSize(imgui.ImVec2(199 * DPI, 0), imgui.Cond.FirstUseEver)
 
         if imgui.Begin("Deprau - Auto Lifefoot", window,
             imgui.WindowFlags.NoCollapse + imgui.WindowFlags.AlwaysAutoResize + imgui.WindowFlags.NoScrollbar) then
+
             imgui.Checkbox("Enable", EnableMain)
-imgui.SameLine()
-        if imgui.Button("Save", imgui.ImVec2(imgui.GetContentRegionAvail().x, 30)) then
-            saveset()
-        end
+            imgui.SameLine()
+            if imgui.Button("Save", imgui.ImVec2(imgui.GetContentRegionAvail().x, 30 * DPI)) then
+                saveset()
+            end
+
             if EnableMain[0] then
                 if imgui.Checkbox("BulletSync", UseBulletSync) then
-                    if UseBulletSync[0] then
-                        UseGiveDamage[0] = false
-                    end
+                    if UseBulletSync[0] then UseGiveDamage[0] = false end
                 end
 
                 if imgui.Checkbox("GiveDamage", UseGiveDamage) then
-                    if UseGiveDamage[0] then
-                        UseBulletSync[0] = false
-                    end
+                    if UseGiveDamage[0] then UseBulletSync[0] = false end
                 end
 
-                imgui.Checkbox("AutoSwitch", EnableWeaponReset)
-
-                imgui.PushItemWidth(250)
+                imgui.PushItemWidth(250 * DPI)
                 imgui.SliderInt("##ResetDuration", ResetDuration, 0, 10000, "Reset - %d")
-
                 if EnableWeaponReset[0] then
                     imgui.SliderInt("##RestoreDelay", RestoreDelay, 0, 10000, "Switch - %d")
                 end
-
                 imgui.PopItemWidth()
             end
         end
